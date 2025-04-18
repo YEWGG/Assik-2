10dd7a418eb7861afdc490f3aead9a040a109522
daure <foraclient6089@gmail.com>
2025-04-19 00:18:48 +0500

    MyLinkedList and MyArrayList test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> myList = new MyArrayList<>();
+        MyArrayList<Integer> myArray = new MyArrayList<>();
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(3);
+        myArray.add(4);
+        myArray.add(5);
+        myArray.add(6);
+        myArray.remove(5);
+        myArray.add(2, 10);
+        myArray.addFirst(0);
+        myArray.addLast(7);
+        myArray.sort(Integer::compareTo);
+        for (int i = 0; i < myArray.size(); i++) {
+            System.out.print(myArray.get(i)+" ");
+        }
+        System.out.println("\n"+"+++++++++++++++++++++++");
+
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+        myLinkedList.add(6);
+        myLinkedList.remove(5);
+        System.out.println(myLinkedList.size());
+
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.print(myLinkedList.get(i)+" ");
+        }
+
     }
     }
